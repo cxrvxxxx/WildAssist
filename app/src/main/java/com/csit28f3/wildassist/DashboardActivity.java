c2c21ab9e5cfdfdae9b893429f34db2f35dcecf3
cxrvxxxx <brentempasisx3@gmail.com>
2023-05-16 22:16:51 +0800

    added edit profile

--- app/src/main/java/com/csit28f3/wildassist/DashboardActivity.java
+++ app/src/main/java/com/csit28f3/wildassist/DashboardActivity.java
@@ -2,13 +2,40 @@ package com.csit28f3.wildassist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView lblSessionUser;
+    private Button btnEditProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        lblSessionUser = findViewById(R.id.lblSessionUser);
+
+        btnEditProfile = findViewById(R.id.btnEditProfile);
+
+        lblSessionUser.setText(Session.getActiveUser().getName());
+
+        btnEditProfile.setOnClickListener(DashboardActivity.this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnEditProfile:
+                Intent intent = new Intent(DashboardActivity.this, ProfileActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
