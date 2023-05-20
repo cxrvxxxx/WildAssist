803c3347fca68c46610e420cdef3e32e54ad63d1
cxrvxxxx <brentempasisx3@gmail.com>
2023-05-20 16:16:42 +0800

    changed booking layout

--- app/src/main/java/com/csit28f3/wildassist/NewBookingActivity.java
+++ app/src/main/java/com/csit28f3/wildassist/NewBookingActivity.java
@@ -3,12 +3,30 @@ package com.csit28f3.wildassist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class NewBookingActivity extends AppCompatActivity {
+    private AutoCompleteTextView txtNewBookingDestination;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_booking);
+
+        txtNewBookingDestination = findViewById(R.id.txtNewBookingDestination);
+
+        String[] optionsArray = {
+                "Main Bldg.",
+                "ST Bldg.",
+                "GLE Bldg.",
+                "Academic Bldg.",
+                "BED Bldg.",
+                "Gymnasium",
+                "Other"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(NewBookingActivity.this, android.R.layout.simple_dropdown_item_1line, optionsArray);
+        txtNewBookingDestination.setAdapter(adapter);
     }
 }
